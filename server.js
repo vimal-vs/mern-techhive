14714b2e10b41c302338fd8938671aec84d08d00
Vimal Vs <vimalchan23@gmail.com>
2023-08-04 22:40:53 +0530

    removed spacing

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-app.use(cors());
 
 app.set('port',3001);
 
